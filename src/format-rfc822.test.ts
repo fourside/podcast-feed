75421b8f61ce78272cc88da2209e6da77ee4bf0f
fourside <fourside@gmail.com>
2023-12-23 00:26:50 +0900

    in source testing / fix TZ

--- src/format-rfc822.test.ts
+++ src/format-rfc822.test.ts
@@ -2,12 +2,21 @@ import { describe, expect, test } from "vitest";
 import { formatRfc822 } from "./format-rfc822";
 
 describe(formatRfc822.name, () => {
-  test("format", () => {
+  test("time has zero padding", () => {
     // arrange
     const date = new Date(2022, 2, 1, 3, 4, 5);
     // act
     const result = formatRfc822(date);
     // assert
     expect(result).toBe("Tue, 1 Mar 2022 03:04:05 +0000");
+  });
+
+  test("after 12 hour", () => {
+    // arrange
+    const date = new Date(2022, 2, 22, 13, 14, 15);
+    // act
+    const result = formatRfc822(date);
+    // assert
+    expect(result).toBe("Tue, 22 Mar 2022 13:14:15 +0000");
   });
 });
--- src/format-rfc822.ts
+++ src/format-rfc822.ts
@@ -14,21 +14,19 @@ const MONTHS = [
   "Dec",
 ] as const;
 
+const timeFormatter = new Intl.DateTimeFormat("ja-JP", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
+
 export function formatRfc822(date: Date): string {
   // Sat, 16 Dec 2023 10:30:59 +0000
   const dayOfWeek = DAYS_OF_WEEK[date.getDay()];
   const month = MONTHS[date.getMonth()];
+  const time = timeFormatter.format(date);
   const timezone = calcTimezone(date.getTimezoneOffset());
-  return `${dayOfWeek}, ${date.getDate()} ${month} ${date.getFullYear()} ${pad(
-    date.getHours(),
-  )}:${pad(date.getMinutes())}:${pad(date.getSeconds())} ${timezone}`;
-}
-
-function pad(num: number): string {
-  if (num < 10) {
-    return `0${num}`;
-  }
-  return num.toString();
+  return `${dayOfWeek}, ${date.getDate()} ${month} ${date.getFullYear()} ${time} ${timezone}`;
 }
 
 function calcTimezone(offsetMin: number): string {
@@ -37,4 +35,44 @@ function calcTimezone(offsetMin: number): string {
   const modMin = absMin % 60;
   const sign = offsetMin <= 0 ? "+" : "-";
   return `${sign}${pad(offsetHour)}${pad(modMin)}`;
+}
+
+function pad(num: number): string {
+  if (num < 0) {
+    throw new Error("use Math.abs");
+  }
+  const numString = num.toString();
+  return numString.length < 2 ? `0${numString}` : numString;
+}
+
+if (import.meta.vitest !== undefined) {
+  const { describe, test, expect } = import.meta.vitest;
+  describe(calcTimezone.name, () => {
+    test("negative", () => {
+      // arrange
+      const offsetMin = -245;
+      // act
+      const result = calcTimezone(offsetMin);
+      // assert
+      expect(result).toBe("+0405");
+    });
+
+    test("positive", () => {
+      // arrange
+      const offsetMin = 123;
+      // act
+      const result = calcTimezone(offsetMin);
+      // assert
+      expect(result).toBe("-0203");
+    });
+
+    test("zero", () => {
+      // arrange
+      const offsetMin = 0;
+      // act
+      const result = calcTimezone(offsetMin);
+      // assert
+      expect(result).toBe("+0000");
+    });
+  });
 }
