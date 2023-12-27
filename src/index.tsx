4d40776e4a192fa5f23a45270b65263633444441
fourside <fourside@gmail.com>
2023-12-27 22:28:40 +0900

    crud d1

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
 import { html } from "hono/html";
+import { object, string, validate, type Describe } from "superstruct";
 import { FILE_PATH, Feed } from "./feed";
 
 type Bindings = {
   PRIVATE_PODCAST: R2Bucket;
+  DB: D1Database;
   HOST: string;
   USERNAME: string;
   PASSWORD: string;
@@ -43,6 +45,79 @@ app.get(`${FILE_PATH}/:file`, async (c) => {
     const buff = await file.arrayBuffer();
     await stream.write(new Uint8Array(buff));
   });
+});
+
+interface ProgramModel {
+  stationId: string;
+  title: string;
+  fromTime: string; // yyyymmddHHmm
+  duration: string; // min
+  personality: string;
+}
+
+interface ProgramRecord extends ProgramModel {
+  id: string;
+}
+
+app.get("/tasks", async (c) => {
+  const statement = c.env.DB.prepare(
+    "SELECT * FROM Tasks ORDER BY createdAt DESC",
+  );
+  const result = await statement.all<ProgramRecord>();
+  if (result.success) {
+    return c.json(JSON.stringify(result.results));
+  }
+  console.error(result.error);
+  return c.json({ message: "error" }, 500);
+});
+
+const programSchema: Describe<ProgramModel> = object({
+  stationId: string(),
+  title: string(),
+  fromTime: string(),
+  duration: string(),
+  personality: string(),
+});
+
+app.post("/task", async (c) => {
+  const json = await c.req.json();
+  const [err, program] = validate(json, programSchema);
+  if (err !== undefined) {
+    return c.json({ message: JSON.stringify(err) }, 400);
+  }
+  const id = crypto.randomUUID();
+  const statement = c.env.DB.prepare(
+    `INSERT INTO Tasks
+      (id, stationId, title, fromTime, duration, personality)
+    VALUES
+      (?1, ?2, ?3, ?4, ?5, ?6)`,
+  ).bind(
+    id,
+    program.stationId,
+    program.title,
+    program.fromTime,
+    program.duration,
+    program.personality,
+  );
+  const result = await statement.run();
+  if (result.success) {
+    return c.json({ message: "success" }, 201);
+  }
+  console.error(result.error);
+  return c.json({ message: "error" }, 500);
+});
+
+app.delete("/tasks/:id", async (c) => {
+  const id = c.req.param("id");
+  const statement = c.env.DB.prepare("DELETE FROM Tasks WHERE id = ?1").bind(
+    id,
+  );
+  const result = await statement.run();
+  if (result.success) {
+    return c.json({ message: "success" });
+  }
+  console.error(result.error);
+  return c.json({ message: "error" }, 500);
 });
 
 export default app;
